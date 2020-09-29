5e3928ea46986d5a9db5dd991530e166bef536a0
vladz <vladzyaba@gmail.com>
2020-09-29 16:07:31 +0300

    added Navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import classes from './Header.module.scss'
-import {NavLink} from 'react-router-dom'
-import * as LINKS from '../../route'
+import Navbar from '../Navbar/Navbar'
 
 const Header = () => {
    return (
       <header className={classes.header}>
          <div className={classes.container}>
             <div>Logo</div>
-            <nav>
-               <NavLink to={LINKS.LOGIN}>login</NavLink>
-               <NavLink to={LINKS.PROFILE}>profile</NavLink>
-               <NavLink to={LINKS.REGISTRATION}>registration</NavLink>
-               <NavLink to={LINKS.RESTORE_PASSWORD}>restore password</NavLink>
-            </nav>
+            <Navbar/>
          </div>
       </header>
    )
