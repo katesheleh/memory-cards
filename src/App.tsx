a0f6e44ba7433c9c526f597871c53b9d69aacd5f
vladz <vladzyaba@gmail.com>
2020-10-01 00:05:24 +0300

    fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,51 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import './App.css'
 import {Route, Switch} from 'react-router-dom'
-import {CARDS, LOGIN, NEW_PASSWORD, PACKS, PROFILE, REGISTRATION, RESTORE_PASSWORD} from './route'
+import * as paths from './route'
 import Login from './components/Login/Login'
-import NewPassword from './components/NewPassword/NewPassword'
 import Profile from './components/Profile/Profile'
-import Registration from './components/Registration/Registration'
-import RestorePassword from './components/RestorePassword/RestorePassword'
 import Header from './components/Header/Header'
-import NotFound from './components/NotFound/NotFound'
 import Cards from "./components/Cards/Cards";
 import Packs from "./components/Packs/Packs";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from "./reducers/store";
+import {authSucessTC} from './reducers/login-reducer'
+import {Preloader} from './components/common'
+
+const Registration = React.lazy(() => import('./components/Registration/Registration'))
+const RestorePassword = React.lazy(() => import('./components/RestorePassword/RestorePassword'))
+const NewPassword = React.lazy(() => import('./components/NewPassword/NewPassword'))
+const NotFound = React.lazy(() => import('./components/NotFound/NotFound'))
 
 function App() {
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.login.isLoggedIn)
+    const requestIsFetching = useSelector<AppRootStateType, boolean>(state => state.request.isFetching)
+    const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        dispatch(authSucessTC())
+    }, [])
+
     return (
         <div className="App">
+            {requestIsFetching && <Preloader/>}
             <Header/>
             <div className="content">
-                <Switch>
-                    <Route path={LOGIN} render={() => <Login/>}/>
-                    <Route path={NEW_PASSWORD} render={() => <NewPassword/>}/>
-                    <Route path={PROFILE} render={() => <Profile/>}/>
-                    <Route path={REGISTRATION} render={() => <Registration/>}/>
-                    <Route path={RESTORE_PASSWORD} render={() => <RestorePassword/>}/>
-                    <Route path={PACKS} render={() => <Packs/>}/>
-                    <Route path={CARDS} render={() => <Cards/>}/>
-                    <Route path='/' render={() => isLoggedIn ? <Profile/> : <Login/>}/>
-                    <Route component={NotFound}/>
-                </Switch>
+                <Suspense fallback={<Preloader/>}>
+                    <Switch>
+                        <Route exact path='/' render={() => isLoggedIn ? <Profile/> : <Login/>}/>
+                        <Route path={paths.LOGIN} render={() => <Login/>}/>
+                        <Route path={paths.PROFILE} render={() => <Profile/>}/>
+                        <Route path={paths.PACKS} render={() => <Packs/>}/>
+                        <Route path={paths.CARDS} render={() => <Cards/>}/>
+
+                        <Route path={paths.REGISTRATION} render={() => <Registration/>}/>
+                        <Route path={paths.RESTORE_PASSWORD} render={() => <RestorePassword/>}/>
+                        <Route path={paths.NEW_PASSWORD} render={() => <NewPassword/>}/>
+
+                        <Route component={NotFound}/>
+                    </Switch>
+                </Suspense>
             </div>
         </div>
     )
